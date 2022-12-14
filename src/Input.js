75cdbe01d46a35ac3d5cb41b886e4304ae3a7775
Daniel Boyce <daarf92@gmail.com>
2022-12-14 21:38:46 +0000

    Init

---
+++ src/Input.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const Input = () => {
+
+    return(
+        <div className ='input'>
+            Hello
+        </div>
+    )
+}
+
+export default Input;
